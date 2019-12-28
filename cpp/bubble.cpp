aa5b1d5295430fbfa98e3f276233ebd98839d177
Nicholas Grogg <oddone9139@OldBoy>
2019-12-28 10:37:42 -1000

    Finished Bubble Sort in C++

--- cpp/bubble.cpp
+++ cpp/bubble.cpp
@@ -3,6 +3,35 @@
 
 using namespace std;
 
+// Be aware this code requires at least std11!
+
 int main(){
+	int n;
+	cout << "How many numbers to sort? ";
+	cin >> n;
+
+	// Array to hold sorted values
+	int values[n];
+
+	// Populate array
+	for (int count = 0; count < n; count++){
+		cin >> values[count];
+	}
+
+	// Sort array
+	for (int i = 0; i < n-1; i++){
+		for (int j = 0; j < n; j++){
+			if (values[j] > values[j+1]){
+				swap(values[j],values[j+1]);
+			}
+		}
+	}
+
+	// Output array
+	for (int count = 0; count < n; count ++){
+		cout << values[count] << " ";
+	}
+	cout << endl;
+
 	return 0;
 }
