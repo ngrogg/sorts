c1544b7af2129ea222c9022c2e3814531c1cd1e7
Nicholas Grogg <oddone9139@OldBoy>
2019-12-28 10:57:01 -1000

    Added Bubble Sort in python

--- python/bubble.py
+++ python/bubble.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+
+print("Enter number of elements to sort")
+n = int(input())
+
+print("Enter elements to sort as space delimited line ex. '1 4 2 3'")
+v     = str(input())
+vList = v.split(' ')
+
+# Output unsorted 
+print(vList)
+
+# Bubble sort
+for i in range(n):
+    for j in range(0,n-i-1):
+        if int(vList[j]) > int(vList[j+1]):
+            vList[j], vList[j+1] = vList[j+1], vList[j]
+
+# Output sorted
+print(vList)
